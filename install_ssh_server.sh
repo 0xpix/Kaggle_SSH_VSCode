7cf1b9a7459f691a97df3f38d7a4a5b452600349
0xpix <ns.chlfat@gmail.com>
2025-03-08 18:34:45 +0100

    Refactor SSH server installation script to improve key handling and configuration

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -1,30 +1,35 @@
-#!/bin/bash#
+#!/bin/bash
+
 # Setup public - private key
 mkdir -p /kaggle/working/.ssh
 echo $1
 FILE=/kaggle/working/.ssh/authorized_keys
-if test -f "$FILE"; 
-then
-    wget $1 -O /kaggle/working/.ssh/temp
+
+# Download the public key
+if test -f "$FILE"; then
+    wget -O /kaggle/working/.ssh/temp "$1"
     cat /kaggle/working/.ssh/temp >> /kaggle/working/.ssh/authorized_keys
     rm /kaggle/working/.ssh/temp
 else
-    wget $1 -O /kaggle/working/.ssh/authorized_keys
+    wget -O /kaggle/working/.ssh/authorized_keys "$1"
 fi
 
+# Set correct permissions
 chmod 700 /kaggle/working/.ssh
 chmod 600 /kaggle/working/.ssh/authorized_keys
 
 # Install SSH-Server
-sudo apt update
-sudo apt install openssh-server -y
+sudo apt update -qq
+sudo apt install -y openssh-server
 
 # SSH Config
-echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config
-echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
-echo "AuthorizedKeysFile /kaggle/working/.ssh/authorized_keys" | sudo tee -a /etc/ssh/sshd_config
-echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+sudo tee -a /etc/ssh/sshd_config <<EOF
+Port 22
+PermitRootLogin yes
+PasswordAuthentication no
+AuthorizedKeysFile /kaggle/working/.ssh/authorized_keys
+PubkeyAuthentication yes
+EOF
 
-sudo service ssh stop
-sudo /usr/sbin/sshd -D &
-
+# Restart SSH service
+sudo service ssh restart
